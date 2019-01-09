2747ed32ded0e4022d1a146f3c979f6893a9e714
Nathaniel Johnson <nate.johnson@b507.us>
2019-01-09 00:05:18 -0600

    Configuration Manager fully implemented. Changes made to auto_docker parsing the dictionary instead of a Namespace was made...TODO...need to update how arguments are saved in regards to the new feature.

--- auto_docker.py
+++ auto_docker.py
@@ -5,7 +5,7 @@ from paramiko import SSHClient, AutoAddPolicy
 from localDocker import host_docker, non_host_docker
 from serverDocker import server_host_docker, server_non_host_docker
 from client import open_client
-from configs import config_file
+from configs import config_file, config_manager
 def main():
     # initialize global variables
     host = False
@@ -68,35 +68,57 @@ def main():
     if args.config == True:
         config_file(args)
 
+    # Assign Arguments to Variables Considering if the config manager was used.
+    if args.config_manager == True:
+        args = config_manager()
+        host = args['host']
+        dns = args['dns']
+        name = args['name']
+        repo = args['repo']
+        volume = args['volume']
+        vol_target = args['volume_target']
 
-    # Assign Arguments to Variables
-    host = args.host
-    dns = args.dns
-    name = args.name
-    repo = args.repo
-    volume = args.repo
-    vol_target = args.volume_target
+        share_path = args['share_path']
+        share_path_target = args['share_path_target']
 
-    share_path = args.share_path
-    share_path_target = args.share_path_target
+        ssh_host = args['ssh_host']
+        ssh_port = args['ssh_port']
+        ssh_user = args['ssh_user']
+        if args['ssh_pass']is not None:
+            ssh_pass = args['ssh_pass'].replace('\'', '')
+        shadow = args['shadow']
+    else:
+        host = args.host
+        dns = args.dns
+        name = args.name
+        repo = args.repo
+        volume = args.volume
+        vol_target = args.volume_target
 
-    ssh_host = args.ssh_host
-    ssh_port = args.ssh_port
-    ssh_user = args.ssh_user
-    ssh_pass = args.ssh_pass.replace('\'', '')
-    shadow = args.shadow
+        share_path = args.share_path
+        share_path_target = args.share_path_target
+
+        ssh_host = args.ssh_host
+        ssh_port = args.ssh_port
+        ssh_user = args.ssh_user
+        if args.ssh_pass is not None:
+            ssh_pass = args.ssh_pass.replace('\'', '')
+        shadow = args.shadow
+
+
+
     # Check to see if all SSH arguments are present in memory before creating a client. Kill if partial arguments.
     # Proceed on local machine if none are present
-    if ssh_host and ssh_user and ssh_pass:
+    if ssh_host is not None and ssh_user is not None and ssh_pass is not None:
         if ssh_port is None:
             ssh_port = 22
         print("Auto Docker Running on Remote Server")
 
         channel = open_client(ssh_host, ssh_port, ssh_user, ssh_pass)
-    elif ssh_host is None or ssh_port is None or ssh_user is None or ssh_pass is None:
-        print("All SSH arguments are required to use SSH.")
-        print("Auto docker will exit now...")
-        exit()
+    # elif ssh_host is None or ssh_port is None or ssh_user is None or ssh_pass is None:
+    #     print("All SSH arguments are required to use SSH.")
+    #     print("Auto docker will exit now...")
+    #     exit()
     else:
         print("Auto Docker Running on Local Machine")
 
--- configs.py
+++ configs.py
@@ -3,12 +3,31 @@ import json
 
 
 def config_manager():
+    # TODO: create config manager to print out configurations and to be selected by the user
     config_name = None
+    configs = [f for f in os.listdir("configs") if os.path.isfile(os.path.join("configs", f))]
 
-    onlyfiles = [f for f in os.listdir("configs") if os.path.isfile(os.path.join("configs", f))]
+    if len(configs) == 0:
+        print("No Configurations saved.\n"
+              "Use --config to save a configuartion")
+    else:
+        print("Configurations")
+        config_args = {}
+        config_number = 0
+        for config in configs:
+            config_number += 1
+            print("\t{0} - {1}".format(config, config_number))
 
-    list_config(config_name)
-    """Todo: create config manager to print out configurations and to be selected by the user"""
+        config_selection = input("Select a Config: ")
+
+        with open('configs/' + configs[int(config_selection ) - 1]) as config_file:
+            json_config = json.load(config_file)
+
+
+            for key, value in json_config.items():
+                config_args[key] = value
+        return config_args
+
 
 
 def config_file(args):
@@ -20,9 +39,14 @@ def config_file(args):
             if os.path.isfile(config_file_name) == False:
                 open('configs/{0}'.format(config_file_name), 'x')
     with open('configs/{0}'.format(config_file_name), 'a') as config_file:
+        arg_to_be_saved = {}
         arg_dictionary = vars(args)
+        arg_to_be_saved = arg_dictionary
 
-        config_file.write("{0}\n".format(json.dumps(arg_dictionary)))
+        #remove config arguments from saved configuration
+
+        #write to configuration file
+        config_file.write("{0}\n".format(json.dumps(arg_to_be_saved)))
         print('Wrote {0} to config file'.format(config_file_name))
 
         config_file.close()
@@ -38,3 +62,9 @@ def list_config(config_file_name="configs.txt"):
         print(json.load(config_file))
 
         print(configs)
+
+def load_config(config_file_name):
+    config_file = json.load(config_file_name)
+
+    return config_file
+
